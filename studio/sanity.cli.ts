9df5db391ee7e8dd48f1c8dcadc52af890cae17d
hyo <colleterhyosua@gmail.com>
2026-09-11 08:15:17 +0200

    chore: ajoute appId sanity studio deploye

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -14,5 +14,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    appId: 'bm6viz8mzufl8yrklimddh8y',
   }
 })
